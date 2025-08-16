62dee73b67d5071b90d8b676df5c90ec8bc1d0f8
DiaaAnadani <diaaanadani369@gmail.com>
2025-08-16 12:23:15 +0300

    edit flavor

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("dev.flutter.flutter-gradle-plugin") // Flutter plugin يجب أن يكون الأخير
 }
 
 android {
@@ -20,10 +19,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.doctor_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,30 +28,29 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-    android {
-    ...
-    buildTypes {
-      getByName("debug") {...}
-      getByName("release") {...}
-    }
-    ...
     flavorDimensions += "default"
     productFlavors {
         create("staging") {
             dimension = "default"
+            resValue(
+                type = "string",
+                name = "app_name",
+                value = "Flavors staging")
             applicationIdSuffix = ".staging"
         }
         create("production") {
             dimension = "default"
+            resValue(
+                type = "string",
+                name = "app_name",
+                value = "Flavors production")
             applicationIdSuffix = ".production"
         }
     }
-}
+
 }
 
 flutter {
--- lib/features/sign_up/ui/widgets/sign_up_form.dart
+++ lib/features/sign_up/ui/widgets/sign_up_form.dart
@@ -4,6 +4,7 @@ import 'package:doctor_app/core/helpers/app_regex.dart';
 import 'package:doctor_app/core/helpers/spacing.dart';
 import 'package:doctor_app/core/widgets/app_text_form_field.dart';
 import 'package:doctor_app/features/login/ui/widgets/password_validations.dart';
+import 'package:doctor_app/features/sign_up/logic/cubit/sign_up_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
